a5332aa50a606f7b2dd635fb08bc957b49b4749c
Yuan-Yi Chang <changyy.csie@gmail.com>
2024-07-18 13:39:06 +0800

    Update scripts

--- run-develop.sh
+++ run-develop.sh
@@ -19,7 +19,7 @@ fi
 
 # Check if /tmp directory exists
 if [ ! -d "./tmp" ]; then
-    echo "Creating /tmp directory..."
+    echo "Creating ./tmp directory..."
     mkdir ./tmp
 fi
 
@@ -41,28 +41,44 @@ else
     echo "./tmp/osTicket directory already exists."
 fi
 
+# Check if ./tmp/osTicket/stage directory exists
+if [ ! -d "./tmp/osTicket/stage" ]; then
+    echo "Creating ./tmp/osTicket/stage directory..."
+    mkdir ./tmp/osTicket/stage
+fi
+
 # Check if ./tmp/osTicket-plugins directory exists
-if [ ! -d "./tmp/osTicket-plugins" ]; then
-    echo "./tmp/osTicket-plugins directory does not exist. Cloning repository..."
+if [ ! -d "./tmp/osTicket/stage/osTicket-plugins" ]; then
+    echo "./tmp/osTicket/stage/osTicket-plugins directory does not exist. Cloning repository..."
 
     # Use the first argument as the repository URL, or use the default URL if not provided
     REPO_URL="${2:-https://github.com/osTicket/osTicket-plugins.git}"
 
     # Clone the repository into /tmp/osTicket
-    git clone "$REPO_URL" ./tmp/osTicket-plugins
+    git clone "$REPO_URL" ./tmp/osTicket/stage/osTicket-plugins
 
     if [ $? -ne 0 ]; then
         echo "Failed to clone the repository. Please check the URL and try again."
         exit 1
     fi
 else
-    echo "./tmp/osTicket-plugins directory already exists."
+    echo "./tmp/osTicket/stage/osTicket-plugins directory already exists."
 fi
+
+echo 
+echo "You can put your plugins in ./tmp/osTicket/stage/XXX and link to ./tmp/osTicket/include/plugins/XXX"
+echo
+echo "% ls ./tmp/osTicket/stage/XXX "
+echo "% cd ./tmp/osTicket/include/plugins && ln -s ../../stage/XXX . && cd -"
+echo
+echo
 
 cp ./tmp/osTicket/include/ost-sampleconfig.php ./tmp/osTicket/include/ost-config.php
 
-echo "Using DB Info:"
+echo "Using DB Info: https://github.com/changyy/osticket-study"
+echo
 echo "  host: 127.0.0.1"
+echo "  db:   osticket_dev"
 echo "  user: developer"
 echo "  pass: 12345678"
 echo
